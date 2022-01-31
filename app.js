a95f2b94aebd3e353f8773c16fba07ebc276b1c0
maytheu <maytheu98@gmail.com>
2022-01-31 19:40:40 +0100

    data types in ts

--- app.js
+++ app.js
@@ -1,1 +1,11 @@
-console.log("Starting out with typescript");
+console.log("Specifics data type in ts");
+var person = {
+    name: "Laue",
+    height: 10,
+    hobbies: ["racing", "reading"]
+};
+console.log(person.name);
+for (var _i = 0, _a = person.hobbies; _i < _a.length; _i++) {
+    var hobby = _a[_i];
+    console.log(hobby.toUpperCase());
+}
--- app.ts
+++ app.ts
@@ -1,1 +1,19 @@
-console.log("Starting out with typescript");
+console.log("Specifics data type in ts");
+
+const person: {
+  name: string;
+  height: number;
+  hobbies: string[];
+  roles: [number, string]; //defining tuple with 2 element array
+} = {
+  name: "Laue",
+  height: 10,
+  hobbies: ["racing", "reading"],
+  roles: [1, "author"],
+};
+
+console.log(person.name);
+
+for (const hobby of person.hobbies) {
+  console.log(hobby.toUpperCase());
+}
