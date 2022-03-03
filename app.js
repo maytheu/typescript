fcb37ee5f5f10d4680cfaf0bce253c8b9b7e440a
maytheu <maytheu98@gmail.com>
2022-03-03 16:01:59 +0100

    html with ts

--- app.js
+++ app.js
@@ -1,12 +1,46 @@
-console.log("other data type");
-var userInput;
-userInput = "anything";
-userInput = 5;
-// this wont work but type any will
-var useName;
-// useName = userInput; error
-// this block return never type
-function generateErr(msg, code) {
-    throw { message: msg, errorCode: code };
+"use strict";
+var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+    return c > 3 && r && Object.defineProperty(target, key, r), r;
+};
+function autobind(_, _2, desc) {
+    const originalName = desc.value;
+    const newDesc = {
+        configurable: true,
+        get() {
+            const boundFn = originalName.bind(this);
+            return boundFn;
+        }
+    };
+    return newDesc;
 }
-generateErr("Testing never type", 404);
+class Project {
+    constructor() {
+        this.projectInputEl = (document.getElementById("project-input"));
+        this.appIdEl = document.getElementById("app");
+        const loadDomNode = document.importNode(this.projectInputEl.content, true);
+        this.formEl = loadDomNode.firstElementChild;
+        this.formEl.id = "user-input";
+        this.titleInput = this.formEl.querySelector("#title");
+        this.descInput = (this.formEl.querySelector("#description"));
+        this.peopleInput = this.formEl.querySelector("#people");
+        this.configureListener();
+        this.attach(this.formEl);
+    }
+    submitBindHandler(e) {
+        e.preventDefault();
+        console.log(this.titleInput.value);
+    }
+    configureListener() {
+        this.formEl.addEventListener("submit", this.submitBindHandler);
+    }
+    attach(el) {
+        this.appIdEl.insertAdjacentElement("afterbegin", el);
+    }
+}
+__decorate([
+    autobind
+], Project.prototype, "submitBindHandler", null);
+const project = new Project();
--- app.ts
+++ app.ts
@@ -1,17 +1,74 @@
-console.log("other data type");
-
-let userInput: unknown;
-userInput = "anything";
-userInput = 5;
-
-// this wont work but type any will
-let useName: string;
-// useName = userInput; error
-
-// this block return never type which will never return anythin
-// an infinite loop is also of never return type
-function generateErr(msg: string, code: number): never {
-  throw { message: msg, errorCode: code };
+// decorators - method
+// autobind decorayor
+function autobind(_:any, _2:string, desc:PropertyDescriptor){
+  // meethod name
+  const originalName=desc.value
+  const newDesc:PropertyDescriptor={
+    configurable:true,
+    get(){
+      // calling bin on the bound function
+      const boundFn = originalName.bind(this)
+      return boundFn
+    }
+  }
+  return newDesc
 }
 
-generateErr("Testing never type", 404);
+class Project {
+  projectInputEl: HTMLTemplateElement;
+  appIdEl: HTMLDivElement;
+  formEl: HTMLFormElement;
+  // input varaible
+  titleInput: HTMLInputElement;
+  peopleInput: HTMLInputElement;
+  descInput: HTMLInputElement;
+
+  // initializing in the constructor to instantiate immediately the instance
+  constructor() {
+    // typecasting in 2 diff ways
+    this.projectInputEl = <HTMLTemplateElement>(
+      document.getElementById("project-input")!
+    );
+    this.appIdEl = document.getElementById("app")! as HTMLDivElement;
+
+    //load form element to dom
+    const loadDomNode = document.importNode(this.projectInputEl.content, true);
+    this.formEl = loadDomNode.firstElementChild as HTMLFormElement;
+
+    // interacting with element by adding id
+    this.formEl.id = "user-input";
+
+    this.titleInput = this.formEl.querySelector("#title") as HTMLInputElement;
+    this.descInput = <HTMLInputElement>(
+      this.formEl.querySelector("#description")
+    );
+    this.peopleInput = <HTMLInputElement>this.formEl.querySelector("#people");
+
+    this.configureListener();
+
+    this.attach(this.formEl);
+  }
+
+  
+  @autobind
+  // bind the event listener method
+  private submitBindHandler(e: Event) {
+    e.preventDefault();
+    console.log(this.titleInput.value);
+  }
+
+
+  // adding event listernier
+  private configureListener() {
+    // this.formEl.addEventListener("submit", this.submitBindHandler.bind(this));
+    // usinf decorators
+    this.formEl.addEventListener("submit", this.submitBindHandler);
+  }
+
+  // renderring element
+  private attach(el: HTMLFormElement) {
+    this.appIdEl.insertAdjacentElement("afterbegin", el);
+  }
+}
+
+const project = new Project();
