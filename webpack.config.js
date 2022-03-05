7ec0402aa5bf1bdcce7d65dc33614e5bea240bf4
maytheu <maytheu98@gmail.com>
2022-03-05 14:59:46 +0100

    initial setup

--- webpack.config.js
+++ webpack.config.js
@@ -6,13 +6,13 @@ module.exports = {
   output: {
     path: path.resolve(__dirname, "dist"),
     filename: "bundle.js",
-    // pubicPath:'dist'
+    // publicPath: 'dist'
   },
   devtool: "inline-source-map",
   module: {
     rules: [{ test: /\.ts$/, use: "ts-loader", exclude: "/node_modules/" }],
   },
   resolve: {
-    extensions: ["ts", "js"],
+    extensions: [".ts", ".js"],
   },
 };
