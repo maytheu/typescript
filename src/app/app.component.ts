4b9c31e3c9daf4502295207cc38b36c8fd6c3378
maytheu <maytheu98@gmail.com>
2022-03-21 14:14:08 +0100

    adding event binding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,8 +9,52 @@ export class AppComponent {
   title = 'pswdgen';
 
   password: string = '';
+  includeLetters: boolean = false;
+  includeNumbers: boolean = false;
+  includeSymbols: boolean = false;
+  passwordLength: number = 0;
 
   onButtonClick() {
-    this.password = 'text';
+    const letters = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    const symbols = '!@#$%^&*()_';
+    const number = '1234567890';
+
+    let validChars = '';
+    if (this.includeLetters) {
+      validChars += letters;
+    }
+    if (this.includeNumbers) {
+      validChars += number;
+    }
+    if (this.includeSymbols) {
+      validChars += symbols;
+    }
+
+    let generatedPassword = '';
+    for (let i = 0; i < this.passwordLength; i++) {
+      const ind = Math.floor(Math.random() * validChars.length);
+      generatedPassword += validChars[ind];
+    }
+
+    this.password = generatedPassword;
+  }
+
+  addLetters() {
+    this.includeLetters = !this.includeLetters;
+  }
+
+  addSymbols() {
+    this.includeSymbols = !this.includeSymbols;
+  }
+
+  addNumbers() {
+    this.includeNumbers = !this.includeNumbers;
+  }
+
+  inputLength(val: any) {
+    const parseVal = +val.value;
+    if (!isNaN(parseVal)) {
+      this.passwordLength = parseVal;
+    }
   }
 }
