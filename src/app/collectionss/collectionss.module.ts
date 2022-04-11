3855e30009119dc7b37e258f8f24412c816d6483
maytheu <maytheu98@gmail.com>
2022-04-11 10:00:57 +0100

    router links

--- src/app/collectionss/collectionss.module.ts
+++ src/app/collectionss/collectionss.module.ts
@@ -5,9 +5,12 @@ import { CollectionssRoutingModule } from './collectionss-routing.module';
 import { CollectionsHomeComponent } from './collections-home/collections-home.component';
 import { WidgetsModule } from '../widgets/widgets.module';
 import { CollectionTableComponent } from './collection-table/collection-table.component';
+import { BiographyComponent } from './biography/biography.component';
+import { CompaniesComponent } from './companies/companies.component';
+import { PartnerComponent } from './partner/partner.component';
 
 @NgModule({
-  declarations: [CollectionsHomeComponent, CollectionTableComponent],
+  declarations: [CollectionsHomeComponent, CollectionTableComponent, BiographyComponent, CompaniesComponent, PartnerComponent],
   imports: [CommonModule, CollectionssRoutingModule, WidgetsModule],
 })
 export class CollectionssModule {}
